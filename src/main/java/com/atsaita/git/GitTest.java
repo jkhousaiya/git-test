077da4f7476db6cbbba2fee9828601201d3c6951
侯赛亚 <1125842156@qq.com>
2023-03-14 17:10:25 +0800

    hot-fix commit

--- src/main/java/com/atsaita/git/GitTest.java
+++ src/main/java/com/atsaita/git/GitTest.java
@@ -11,5 +11,7 @@ public class GitTest {
  public static void main(String[] args) {
   System.out.println("hello git!");
   System.out.println("hello git!");
+  System.out.println("hello git!1111");
+  System.out.println("hello git!2222");
  }
 }
