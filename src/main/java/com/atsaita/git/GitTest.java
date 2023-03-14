3c5ea0263e2095c6006843df47276694d545aa31
侯赛亚 <1125842156@qq.com>
2023-03-14 18:55:28 +0800

    push commit

--- src/main/java/com/atsaita/git/GitTest.java
+++ src/main/java/com/atsaita/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
   System.out.println("master text!");
   System.out.println("hello git!1111");
   System.out.println("hello git!2222");
+  System.out.println("push git!3333");
  }
 }
