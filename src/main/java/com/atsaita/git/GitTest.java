22999fdebd7c89ebe0bd722c8f4114501a4765f0
侯赛亚 <1125842156@qq.com>
2023-03-14 16:49:47 +0800

    first commit

---
+++ src/main/java/com/atsaita/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atsaita.git;
+
+
+
+/**
+ * @Description
+ * @Author Hsy
+ * @Date 2023/3/14 16:28
+ */
+public class GitTest {
+ public static void main(String[] args) {
+  System.out.println("hello git!");
+ }
+}
